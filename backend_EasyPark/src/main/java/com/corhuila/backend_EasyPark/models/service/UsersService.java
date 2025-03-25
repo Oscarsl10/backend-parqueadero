41c48a7d3076078fa6dab0139ed3ff9e65901da2
Oscar Sierra <oscarguillermosierralozano@gmail.com>
2025-03-25 08:01:28 -0500

    Cambios

--- backend_EasyPark/src/main/java/com/corhuila/backend_EasyPark/models/service/UsersService.java
+++ backend_EasyPark/src/main/java/com/corhuila/backend_EasyPark/models/service/UsersService.java
@@ -7,6 +7,10 @@ import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 import java.util.Optional;
 
 @Service
@@ -16,20 +20,33 @@ public class UsersService {
     IUsersRepository usersRepository;
 
     public Users addUser(Users user){
+        user.setPassword(hashContrasenia(user.getPassword()));
         return usersRepository.save(user);
     }
 
-    public Boolean loginUser(LoginRequest loginRequest){
+    public String hashContrasenia(String password){
+        try{
+            MessageDigest instancia = MessageDigest.getInstance("SHA-256");
+            byte [] hash = instancia.digest(password.getBytes(StandardCharsets.UTF_8));
+            return HexFormat.of().formatHex(hash);
+        }catch (NoSuchAlgorithmException e){
+            throw new RuntimeException("Error al encriptar la contraseña");
+        }
+    }
 
+    public Boolean loginUser(LoginRequest loginRequest) {
         Optional<Users> user = usersRepository.findById(loginRequest.getUserId());
-        if(user == null){
+
+        if (user.isEmpty()) {
             return false;
         }
 
         Users user1 = user.get();
-        if(!user1.getPassword().equals(loginRequest.getPassword())){
+        // Encripta la contraseña ingresada y compárala con la almacenada
+        if (!user1.getPassword().equals(hashContrasenia(loginRequest.getPassword()))) {
             return false;
         }
+
         return true;
     }
 }
